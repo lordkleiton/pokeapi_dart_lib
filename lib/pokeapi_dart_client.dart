901f04be6c6f5154523195b9e12e9d25e4c75937
lordkleiton <m_s_r@outlook.com>
2020-07-05 19:15:21 -0300

    exportando as classes necessárias

---
+++ lib/pokeapi_dart_client.dart
@@ -0,0 +1,123 @@
+export 'package:pokeapi_dart_client/src/models/berries/berry.dart';
+export 'package:pokeapi_dart_client/src/models/berries/berry_firmness.dart';
+export 'package:pokeapi_dart_client/src/models/berries/berry_flavor.dart';
+export 'package:pokeapi_dart_client/src/models/berries/berry_flavor_map.dart';
+export 'package:pokeapi_dart_client/src/models/berries/flavor_berry_map.dart';
+
+export 'package:pokeapi_dart_client/src/models/common/api_resource.dart';
+export 'package:pokeapi_dart_client/src/models/common/api_resource_list.dart';
+export 'package:pokeapi_dart_client/src/models/common/description.dart';
+export 'package:pokeapi_dart_client/src/models/common/effect.dart';
+export 'package:pokeapi_dart_client/src/models/common/encounter.dart';
+export 'package:pokeapi_dart_client/src/models/common/flavor_text.dart';
+export 'package:pokeapi_dart_client/src/models/common/generation_game_index.dart';
+export 'package:pokeapi_dart_client/src/models/common/machine_version_detail.dart';
+export 'package:pokeapi_dart_client/src/models/common/name.dart';
+export 'package:pokeapi_dart_client/src/models/common/named_api_resource.dart';
+export 'package:pokeapi_dart_client/src/models/common/named_api_resource_list.dart';
+export 'package:pokeapi_dart_client/src/models/common/resource_list.dart';
+export 'package:pokeapi_dart_client/src/models/common/verbose_effect.dart';
+export 'package:pokeapi_dart_client/src/models/common/version_encounter_detail.dart';
+export 'package:pokeapi_dart_client/src/models/common/version_game_index.dart';
+export 'package:pokeapi_dart_client/src/models/common/version_group_flavor_text.dart';
+
+export 'package:pokeapi_dart_client/src/models/contests/contest_effect.dart';
+export 'package:pokeapi_dart_client/src/models/contests/contest_name.dart';
+export 'package:pokeapi_dart_client/src/models/contests/contest_type.dart';
+export 'package:pokeapi_dart_client/src/models/contests/super_contest_effect.dart';
+
+export 'package:pokeapi_dart_client/src/models/encounters/encounter_condition.dart';
+export 'package:pokeapi_dart_client/src/models/encounters/encounter_condition_value.dart';
+export 'package:pokeapi_dart_client/src/models/encounters/encounter_method.dart';
+
+export 'package:pokeapi_dart_client/src/models/evolution/chain_link.dart';
+export 'package:pokeapi_dart_client/src/models/evolution/evolution_chain.dart';
+export 'package:pokeapi_dart_client/src/models/evolution/evolution_detail.dart';
+export 'package:pokeapi_dart_client/src/models/evolution/evolution_trigger.dart';
+
+export 'package:pokeapi_dart_client/src/models/games/generation.dart';
+export 'package:pokeapi_dart_client/src/models/games/pokedex.dart';
+export 'package:pokeapi_dart_client/src/models/games/pokemon_entry.dart';
+export 'package:pokeapi_dart_client/src/models/games/version.dart';
+export 'package:pokeapi_dart_client/src/models/games/version_group.dart';
+
+export 'package:pokeapi_dart_client/src/models/items/item.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_attribute.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_category.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_fling_effect.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_holder_pokemon_version_detail.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_holder_pokemon.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_pocket.dart';
+export 'package:pokeapi_dart_client/src/models/items/item_sprites.dart';
+
+export 'package:pokeapi_dart_client/src/models/languages/language.dart';
+
+export 'package:pokeapi_dart_client/src/models/locations/encounter_method_rate.dart';
+export 'package:pokeapi_dart_client/src/models/locations/encounter_version_details.dart';
+export 'package:pokeapi_dart_client/src/models/locations/location.dart';
+export 'package:pokeapi_dart_client/src/models/locations/location_area.dart';
+export 'package:pokeapi_dart_client/src/models/locations/pal_park_area.dart';
+export 'package:pokeapi_dart_client/src/models/locations/pal_park_encounter_species.dart';
+export 'package:pokeapi_dart_client/src/models/locations/pokemon_encounter.dart';
+export 'package:pokeapi_dart_client/src/models/locations/region.dart';
+
+export 'package:pokeapi_dart_client/src/models/machines/machines.dart';
+
+export 'package:pokeapi_dart_client/src/models/moves/contest_combo_detail.dart';
+export 'package:pokeapi_dart_client/src/models/moves/contest_combo_sets.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_ailment.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_battle_style.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_category.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_damage_class.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_flavor_text.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_learn_method.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_meta_data.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_stat_change.dart';
+export 'package:pokeapi_dart_client/src/models/moves/move_target.dart';
+export 'package:pokeapi_dart_client/src/models/moves/past_move_stat_values.dart';
+
+export 'package:pokeapi_dart_client/src/models/pokemon/ability.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/ability_effect_change.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/ability_flavor_text.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/ability_pokemon.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/awesome_name.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/characteristic.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/egg_group.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/gender.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/genus.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/growth_rate.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/growth_rate_experience_level.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/location_area_ecounter.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/move_battle_style_preference.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/move_stat_affect_sets.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/move_stat_affect.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/nature.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/nature_pokeathlon_stat_affect.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/nature_pokeathlon_stat_affect_sets.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/nature_stat_affect_sets.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/nature_stat_change.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pal_park_encounter_area.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokeathlon_stat.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_ability.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_color.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_form_sprites.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_form.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_habitat.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_held_item.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_held_item_version.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_move.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_move_version.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_shape.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_species.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_species_dex_entry.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_species_gender.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_species_variety.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_sprites.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_stat.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/pokemon_type.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/stat.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/type.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/type_pokemon.dart';
+export 'package:pokeapi_dart_client/src/models/pokemon/type_relations.dart';
